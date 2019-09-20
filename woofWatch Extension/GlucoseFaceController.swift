b21b3c2ed9ad4989f461bba6b29d6696fa473009
Guy Shaviv <guy@tivstudio.com>
2019-09-20 15:59:28 +0300

    enable live preview with time counter

--- woofWatch Extension/GlucoseFaceController.swift
+++ woofWatch Extension/GlucoseFaceController.swift
@@ -12,40 +12,41 @@ import Combine
 
 class CurrentTime: ObservableObject {
     @Published var value = Date()
-}
-
-class GlucoseFaceController: WKHostingController<AnyView> {
-    let currentTime = CurrentTime()
-    var started = false
     var repeater: Repeater?
-    override var body: AnyView {
-        GlucoseFace(state: appState).environmentObject(currentTime).asAnyView
-    }
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        NotificationCenter.default.addObserver(self, selector: #selector(didEnterForeground), name: WKExtension.didEnterBackgroundNotification, object: nil)
-        makeRepeater()
-    }
-    
     func makeRepeater() {
         guard repeater == nil else { return }
-        self.currentTime.value = Date()
+        value = Date()
         repeater = Repeater.every(1.0, leeway: 0.001, queue: DispatchQueue.main, perform: { [weak self] sender in
             if WKExtension.shared().applicationState != .background {
-                self?.currentTime.value = Date()
+                self?.value = Date()
             } else {
                 sender.cancel()
                 self?.repeater = nil
             }
         })
     }
+    init() {
+        makeRepeater()
+    }
+}
+
+class GlucoseFaceController: WKHostingController<AnyView> {
+    let currentTime = CurrentTime()
+    var started = false
+    override var body: AnyView {
+        GlucoseFace(state: appState).environmentObject(currentTime).asAnyView
+    }
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        NotificationCenter.default.addObserver(self, selector: #selector(didEnterForeground), name: WKExtension.didEnterBackgroundNotification, object: nil)
+    }
     
     @objc private func didEnterForeground() {
-        makeRepeater()
+        currentTime.makeRepeater()
     }
     
     override func willActivate() {
         super.willActivate()
-        makeRepeater()
+        currentTime.makeRepeater()
     }
 }
