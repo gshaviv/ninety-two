c1edb4c47f760356984285897df6bed12a7766c6
Guy Shaviv <guy@tivstudio.com>
2019-10-06 14:33:21 +0300

    refactor a1c formula

--- woofWatch Extension/SummaryInfo.swift
+++ woofWatch Extension/SummaryInfo.swift
@@ -161,7 +161,7 @@ class SummaryInfo: ObservableObject {
                 let averageBolus = Storage.default.allEntries.filter { $0.date > start  && $0.date < end }.reduce(0.0) { $0 + Double($1.bolus) } / (end - start) * 1.d
                 
                 let aveG = sumG / totalT
-                let a1c = (aveG / 18.05 + 2.52) / 1.583
+                let a1c = (aveG + 46.7) / 28.7 //(aveG / 18.05 + 2.52) / 1.583
                 let medianLowTime = lowTime.isEmpty ? 0 : Int(lowTime.sorted().median() / 1.m)
                 let timeBelow = lowTime.sum()
                 DispatchQueue.main.async {
