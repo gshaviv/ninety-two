3bb2a84c96fe84200c83cba6ecd30b1c8d351703
Guy Shaviv <guy@tivstudio.com>
2019-12-23 17:18:10 +0200

    always calculated estimated a1c based on 90d

--- woofWatch Extension/SummaryInfo.swift
+++ woofWatch Extension/SummaryInfo.swift
@@ -94,11 +94,11 @@ class SummaryInfo: ObservableObject {
                 MiaoMiao.flushToDatabase()
                 var lowStart: Date?
                 var lowTime = [TimeInterval]()
-                guard let readings = child.evaluate(GlucosePoint.read().filter(GlucosePoint.date > Date() - defaults.summaryPeriod.d).orderBy(GlucosePoint.date)), !readings.isEmpty else {
+                guard let readings = child.evaluate(GlucosePoint.read().filter(GlucosePoint.date > min(Date() - defaults.summaryPeriod.d, Date() - 90.d)).orderBy(GlucosePoint.date)), !readings.isEmpty else {
                     completion?(false)
                     return
                 }
-                let meals = Storage.default.allMeals.filter { $0.date > Date() - defaults.summaryPeriod.d && $0.type != .other }
+                let meals = Storage.default.allMeals.filter { $0.date > Date() - 90.d && $0.type != .other }
                 self.calcDate = Date()
                 var previousPoint: GlucosePoint?
                 var bands = [Int: TimeInterval]()
@@ -112,6 +112,8 @@ class SummaryInfo: ObservableObject {
                 var countLow = false
                 var profile7 = [Double]()
                 var found = false
+                var sum90 = Double(0)
+                var total90 = Double(0)
                 readings.forEach { gp in
                     defer {
                         previousPoint = gp
@@ -121,88 +123,95 @@ class SummaryInfo: ObservableObject {
                         guard duration < 1.h else {
                             return
                         }
-                        if gp.date.hour < 7 {
-                            found = false
-                        }
-                        if !found && gp.date.hour == 23 {
-                            found = true
-                            profile7.append(gp.value)
-                        }
-                        sumG += gp.value * duration
-                        totalT += duration
-                        switch (previous.value, gp.value) {
-                        case (defaults[.maxRange]..., defaults[.maxRange]...):
-                            timeAbove += duration
-                            
-                        case (_, defaults[.maxRange]...):
-                            timeAbove += duration * (gp.value - defaults[.maxRange]) / (gp.value - previous.value)
-                            
-                        case (defaults[.maxRange]..., _):
-                            timeAbove += duration * (previous.value - defaults[.maxRange]) / (previous.value - gp.value)
-                            
-                        default:
-                            break
-                        }
-                        switch (previous.value, gp.value) {
-                        case (180..., 180...):
-                            timeAbove180 += duration
-                            
-                        case (_, 180...):
-                            timeAbove180 += duration * (gp.value - 180) / (gp.value - previous.value)
-                            
-                        case (180..., _):
-                            timeAbove180 += duration * (previous.value - 180) / (previous.value - gp.value)
-                            
-                        default:
-                            break
-                        }
-                        switch (previous.value, gp.value) {
-                        case (..<70, ..<70):
-                            timeBelow70 += duration
-                            
-                        case (_, ..<70):
-                            timeAbove180 += duration * (70 - gp.value) / (previous.value - gp.value)
-                            
-                        case (..<70, _):
-                            timeAbove180 += duration * (70 - previous.value) / (gp.value - previous.value)
-                            
-                        default:
-                            break
-                        }
-                        
-                        maxG = max(maxG, gp.value)
-                        minG = min(minG, gp.value)
-                        let key: Int
-                        switch gp.value {
-                        case ...defaults[.level0]:
-                            key = 0
-                        case ...defaults[.level1]:
-                            key = 1
-                        case ...defaults[.level2]:
-                            key = 2
-                        case ...defaults[.level3]:
-                            key = 3
-                        case ...defaults[.level4]:
-                            key = 4
-                        default:
-                            key = 5
-                        }
-                        bands[key] = (bands[key] ?? 0) + duration
-                        
-                        if gp.value < defaults[.minRange] {
-                            if !inLow {
-                                lowStart = previous.date + (previous.value - defaults[.minRange]) / (previous.value - gp.value) * (gp.date - previous.date)
-                                countLow = gp.value < defaults[.minRange] - 3
+                        if gp.date > Date() - 90.d {
+                            if gp.date.hour < 7 {
+                                found = false
                             }
-                            inLow = true
-                            countLow = countLow || gp.value < defaults[.minRange] - 3
-                        } else {
-                            if inLow, let lowStart = lowStart, countLow {
-                                lowCount += 1
-                                let d = previous.date + (defaults[.minRange] - previous.value) / (gp.value - previous.value) * (gp.date - previous.date)
-                                lowTime.append(d - lowStart)
+                            if !found && gp.date.hour == 23 {
+                                found = true
+                                profile7.append(gp.value)
                             }
-                            inLow = false
+                            sum90 += gp.value * duration
+                            total90 += duration
+                        }
+                        log("gp.date=\(gp.date) summery=\(defaults.summaryPeriod) date-summary=\(Date() - defaults.summaryPeriod.d)")
+                        if gp.date > Date() - defaults.summaryPeriod.d {
+                            sumG += gp.value * duration
+                            totalT += duration
+                            switch (previous.value, gp.value) {
+                            case (defaults[.maxRange]..., defaults[.maxRange]...):
+                                timeAbove += duration
+                                
+                            case (_, defaults[.maxRange]...):
+                                timeAbove += duration * (gp.value - defaults[.maxRange]) / (gp.value - previous.value)
+                                
+                            case (defaults[.maxRange]..., _):
+                                timeAbove += duration * (previous.value - defaults[.maxRange]) / (previous.value - gp.value)
+                                
+                            default:
+                                break
+                            }
+                            switch (previous.value, gp.value) {
+                            case (180..., 180...):
+                                timeAbove180 += duration
+                                
+                            case (_, 180...):
+                                timeAbove180 += duration * (gp.value - 180) / (gp.value - previous.value)
+                                
+                            case (180..., _):
+                                timeAbove180 += duration * (previous.value - 180) / (previous.value - gp.value)
+                                
+                            default:
+                                break
+                            }
+                            switch (previous.value, gp.value) {
+                            case (..<70, ..<70):
+                                timeBelow70 += duration
+                                
+                            case (_, ..<70):
+                                timeAbove180 += duration * (70 - gp.value) / (previous.value - gp.value)
+                                
+                            case (..<70, _):
+                                timeAbove180 += duration * (70 - previous.value) / (gp.value - previous.value)
+                                
+                            default:
+                                break
+                            }
+                            
+                            maxG = max(maxG, gp.value)
+                            minG = min(minG, gp.value)
+                            let key: Int
+                            switch gp.value {
+                            case ...defaults[.level0]:
+                                key = 0
+                            case ...defaults[.level1]:
+                                key = 1
+                            case ...defaults[.level2]:
+                                key = 2
+                            case ...defaults[.level3]:
+                                key = 3
+                            case ...defaults[.level4]:
+                                key = 4
+                            default:
+                                key = 5
+                            }
+                            bands[key] = (bands[key] ?? 0) + duration
+                            
+                            if gp.value < defaults[.minRange] {
+                                if !inLow {
+                                    lowStart = previous.date + (previous.value - defaults[.minRange]) / (previous.value - gp.value) * (gp.date - previous.date)
+                                    countLow = gp.value < defaults[.minRange] - 3
+                                }
+                                inLow = true
+                                countLow = countLow || gp.value < defaults[.minRange] - 3
+                            } else {
+                                if inLow, let lowStart = lowStart, countLow {
+                                    lowCount += 1
+                                    let d = previous.date + (defaults[.minRange] - previous.value) / (gp.value - previous.value) * (gp.date - previous.date)
+                                    lowTime.append(d - lowStart)
+                                }
+                                inLow = false
+                            }
                         }
                     }
                 }
@@ -224,8 +233,9 @@ class SummaryInfo: ObservableObject {
                 let averageBolus = Storage.default.allEntries.filter { $0.date > start  && $0.date < end }.reduce(0.0) { $0 + Double($1.bolus) } / (end - start) * 1.d
                 
                 let aveG = sumG / totalT
+                let ave90 = sum90 / total90
                 // a1c estimation formula based on CGM data: https://care.diabetesjournals.org/content/41/11/2275
-                let a1c = 3.31 + aveG * 0.02392 // (aveG + 46.7) / 28.7 //(aveG / 18.05 + 2.52) / 1.583
+                let a1c = 3.31 + ave90 * 0.02392 // (aveG + 46.7) / 28.7 //(aveG / 18.05 + 2.52) / 1.583
                 let ave7 = profile7.average()
 //                let a1c2 = (aveG + 46.7) / 28.7
                 let a1c3 = (ave7 + 46.7) / 28.7
@@ -235,16 +245,16 @@ class SummaryInfo: ObservableObject {
                 // a1c relationhip to TIR from: https://academic.oup.com/jes/article/3/Supplement_1/SAT-126/5483093/
                 let a1c4 = (157 - tir) / 12.9
                 // based on estimates from: https://care.diabetesjournals.org/content/diacare/31/8/1473.full.pdf
-                let a1c2 = (aveG + 36.9) / 28
+                let a1c2 = (ave90 + 36.9) / 28
                 let a1c5 = (ave7 + 50.7) / 29.1
                 let a1c6 = (ave7 + 43.9) / 28.3
-                let a1c7 = 3.38 + 0.02345 * aveG // https://care.diabetesjournals.org/content/41/11/2275
-                let a1c8 = 3.31 + aveG * 0.02392
-                let a1c9 = 3.15 + 0.02505 * aveG
+                let a1c7 = 3.38 + 0.02345 * ave90 // https://care.diabetesjournals.org/content/41/11/2275
+                let a1c8 = 3.31 + ave90 * 0.02392
+                let a1c9 = 3.15 + 0.02505 * ave90
                 // formulas based on: http://diabetesupdate.blogspot.com/2006/12/formulas-equating-hba1c-to-average.html
-                let a1c10 = (aveG + 77.3) / 35.6
-                let a1c11 = (aveG + 86) / 33.3
-                let a1c12 = (aveG / 18.05 + 2.52) / 1.583
+                let a1c10 = (ave90 + 77.3) / 35.6
+                let a1c11 = (ave90 + 86) / 33.3
+                let a1c12 = (ave90 / 18.05 + 2.52) / 1.583
                 let a1cValues = [a1c, a1c2, a1c3, a1c4, a1c5, a1c6, a1c7, a1c8, a1c9, a1c10, a1c11, a1c12].sorted()
                 let a1cMed = a1cValues.median()
                 let ea1c = Summary.EA1C(value: a1cMed, range: min(a1cValues.percentile(0.75) - a1cMed, a1cMed - a1cValues.percentile(0.25)), cgm: a1c, seven: a1c3, tir: a1c4)
