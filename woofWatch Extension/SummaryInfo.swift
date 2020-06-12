233219c9a0c9594b26e9c62c5dd27e74ae118e82
Guy Shaviv <guy@tivstudio.com>
2020-06-12 14:13:57 +0300

    tweak a1c estimation

--- woofWatch Extension/SummaryInfo.swift
+++ woofWatch Extension/SummaryInfo.swift
@@ -352,6 +352,7 @@ class SummaryInfo: ObservableObject {
                 let ave7 = profile7.average()
 //                let a1c2 = (aveG + 46.7) / 28.7
                 let a1c3 = (ave7 + 46.7) / 28.7
+                let a1c31 = (ave90 + 46.7) / 28.7
                 let medianLowTime = lowTime.isEmpty ? 0 : Int(lowTime.sorted().median() / 1.m)
                 let timeBelow = lowTime.sum()
                 let tir = (total90 - timeBelow70 - timeAbove180) / total90 * 100
@@ -361,14 +362,14 @@ class SummaryInfo: ObservableObject {
                 let a1c2 = (ave90 + 36.9) / 28
                 let a1c5 = (ave7 + 50.7) / 29.1
                 let a1c6 = (ave7 + 43.9) / 28.3
-                let a1c7 = 3.38 + 0.02345 * aveG // https://care.diabetesjournals.org/content/41/11/2275
-                let a1c8 = 3.31 + aveG * 0.02392
+//                let a1c7 = 3.38 + 0.02345 * ave90 // https://care.diabetesjournals.org/content/41/11/2275
+//                let a1c8 = 3.31 + ave90 * 0.02392
                 let a1c9 = 3.15 + 0.02505 * ave90
                 // formulas based on: http://diabetesupdate.blogspot.com/2006/12/formulas-equating-hba1c-to-average.html
                 let a1c10 = (ave90 + 77.3) / 35.6
-                let a1c11 = (ave90 + 86) / 33.3
+//                let a1c11 = (ave90 + 86) / 33.3
                 let a1c12 = (ave90 / 18.05 + 2.52) / 1.583
-                let a1cValues = [a1c, a1c2, a1c3, a1c4, a1c5, a1c6, a1c7, a1c8, a1c9, a1c10, a1c11, a1c12].sorted()
+                let a1cValues = [a1c, a1c2, a1c3, a1c4, a1c5, a1c6,  a1c9, a1c10,  a1c12, a1c31].sorted()
                 let a1cMed = a1cValues.median()
                 let ea1c = Summary.EA1C(value: a1cMed, range: min(a1cValues.percentile(0.75) - a1cMed, a1cMed - a1cValues.percentile(0.25)), cgm: a1c, seven: a1c3, tir: a1c4)
                 DispatchQueue.main.async {
